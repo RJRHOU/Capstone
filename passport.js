9c74dc6fbe1827e041a786c696cf412d014d1130
Victor Brew <huttonbrew@yahoo.com>
2022-07-09 10:35:32 -0500

    pulling

---
+++ passport.js
@@ -0,0 +1,15 @@
+const passport = require("passport");
+const { Passport } = require("passport");
+const GithubStrategy = require('passport-github2')
+
+passport.useCallback(new GithubStrategy({
+    clientID:
+    clientSecret: ""
+    callbackURL:
+
+    Function()
+""}))
+ 
+
+
+Passport
