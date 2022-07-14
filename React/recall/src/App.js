1df36933155bd6e920a1df3bb6820c0818e322f8
Rocky Rosales <rocky.j.rosales@gmail.com>
2022-07-14 17:47:56 -0500

    api working

--- React/recall/src/App.js
+++ React/recall/src/App.js
@@ -1,15 +1,33 @@
 
 import './App.css';
 import Header from './Components/Navbar';
+import { useState } from 'react';
+import Recall from './Components/Recall';
 
-import Products from './Components/Recall';
+// import SearchBox from './Components/Searchbox';
+import { BrowserRouter as Router, Switch, Route, Redirect, } from "react-router-dom";
+import Home from './Components/Home';  
+// import Recall from './Components/Recall';
+
 
 function App() {
+  const [searchValue, setSearchValue] = useState("");
+  
   return (
     
     <div className="App">
-      <Header />
-      <Products />
+      <Header searchValue={searchValue} setSearchValue={setSearchValue}
+ />
+      {/* <Home/> */}
+       {/* <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route exact path='/recall' component={Recall}/>
+         <Route exact path='/submit' component={Submit}/>
+        <Route exact path='/signIn' component={Signin}/>
+        
+       </Switch>  */}
+
+      <Recall searchValue={searchValue} setSearchValue={setSearchValue} />
       
     </div>
   );
--- React/recall/src/Components/Navbar.js
+++ React/recall/src/Components/Navbar.js
@@ -1,69 +1,98 @@
-import React from 'react'
-import Navbar from 'react-bootstrap/Navbar';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Nav from 'react-bootstrap/Nav';
-import Container from 'react-bootstrap/Container';
-import Button  from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form'
-import NavDropdown from 'react-bootstrap/NavDropdown'
-import FormControl from 'react-bootstrap/FormControl'
-import myLogo from './Images/RADish.png'
-
+import React, { useState } from "react";
+import Navbar from "react-bootstrap/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import FormControl from "react-bootstrap/FormControl";
+import myLogo from "./Images/RADish.png";
+// import SearchBox from './Searchbox';
+import { Link } from "react-router-dom";
 
 export default function Header(props) {
+  function handleButtonClick(e) {
+    e.preventDefault();
+    const searchInput = document.getElementById("search-input").value;
+    props.setSearchValue(searchInput);
+    
+  }
+
+  const [query, setQuery] = useState("");
+
   return (
     <div>
-        <Navbar bg="primary" expand="lg">
-  <Container fluid>
-    <Navbar.Brand href="#" >Recall
-    <img
-          
-          src={myLogo}
-          width="80"
-          height="80"
-          className="d-inline-block align-top"
-          alt="React Bootstrap logo"
-          bg="dark"
-
-        />
-      </Navbar.Brand>
-    <Navbar.Toggle aria-controls="navbarScroll" />
-    <Navbar.Collapse id="navbarScroll">
-      <Nav
-        className="me-auto my-2 my-lg-0"
-        style={{ maxHeight: '100px' }}
-        navbarScroll
-      >
-        <Nav.Link href="#action1">Home</Nav.Link>
-        <Nav.Link href="#action2">Link</Nav.Link>
-        <NavDropdown title="Link" id="navbarScrollingDropdown">
-          <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action5">
-            Something else here
-          </NavDropdown.Item>
-        </NavDropdown>
-        <Nav.Link href="#" disabled>
-          Link
-        </Nav.Link>
-      </Nav>
-      <Form className="d-flex">
-        <FormControl
-          type="search"
-          placeholder="State, Store or Product"
-          className="me-2"
-          aria-label="Search"
-        />
-        <Button variant="light" >Search</Button>
-      </Form>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
+      <Navbar bg="primary" expand="lg">
+        <Container fluid>
+          {/* <Link to='/'> */}
+          <Navbar.Brand href="#">
+            Recall
+            <img
+              src={myLogo}
+              width="80"
+              height="80"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+              bg="dark"
+            />
+          </Navbar.Brand>
+          {/* </Link>  */}
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              {/* <Link to='/recall'> */}
+              <Nav.Link href="#action1">Recall</Nav.Link>
+              {/* </Link>  */}
+              <Nav.Link href="#action2">Link</Nav.Link>
+              <NavDropdown title="Link" id="navbarScrollingDropdown">
+                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action4">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action5">
+                  Something else here
+                </NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="#" disabled>
+                Link
+              </Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <FormControl
+                type="search"
+                placeholder="State, Store or Product"
+                className="me-2"
+                aria-label="Search"
+                onChange={(e) => setQuery(e.target.value)}
+              />
+              <Button variant="light">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+          <form className="d-flex ms-auto me-1">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+              id="search-input"
+            />
+            <button
+              onClick={handleButtonClick}
+              className="btn btn-outline-light"
+              type="submit"
+              id="search-button"
+            >
+              Search
+            </button>
+          </form>{" "}
+        </Container>
+      </Navbar>
     </div>
-  )
+  );
 }
-
-
-
-
